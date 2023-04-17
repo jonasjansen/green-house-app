a5487b789eeff11ad7068d9cf94ca04dfbb0481d
Jonas Jansen <git@jonasjansen.de>
2023-04-17 15:27:01 +0200

    Initial Commit

---
+++ green-house-app/green_house_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  green_house_appApp.swift
+//  green-house-app
+//
+//  Created by Jonas Jansen on 17.04.23.
+//
+
+import SwiftUI
+
+@main
+struct green_house_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
